3cbb0b4b7e5f1e95ed8368ded6a4648e59222bf6
Masao Kitamura <masaok@gmail.com>
2023-09-13 22:08:07 -0700

    Initial Commit

---
+++ watchos-healthkit-demo Watch App/watchos_healthkit_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchos_healthkit_demoApp.swift
+//  watchos-healthkit-demo Watch App
+//
+//  Created by keckadmin on 9/13/23.
+//
+
+import SwiftUI
+
+@main
+struct watchos_healthkit_demo_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
